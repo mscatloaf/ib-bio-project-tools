f55ddc3ffb9d1d78a769a5c34a90807da8b2c352
catloaf <80300628+mscatloaf@users.noreply.github.com>
2025-01-30 17:14:25 -0800

    updated files, histogram a has real data

--- histogram-a.py
+++ histogram-a.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 fig, ax = plt.subplots()
 
 xAxis = ['0.5', '1.0', '2.0', '4.0', '8.0']
-yAxis = [12.646038, 20, 20, 20, 33.395914]
+yAxis = [11.270115, 18.586385, 29.09563833, 28.27753, 33.166978]
+errors = [2.066325443, 5.977915501, 11.9942207, 16.06650263, 16.62635611]
 
 bar_labels = ['red', 'blue', 'yellow', 'orange', 'purple']
 bar_colors = ['tab:red', 'tab:blue', 'tab:olive', 'tab:orange', 'tab:purple']
@@ -15,8 +16,6 @@ ax.set_ylabel('Average rate of CO2 Uptake / ppm * sec^-1')
 ax.set_title('Average rate of CO2 uptake for a given amount of sucrose, \nerror bars represent second standard deviation.')
 
 #ax.legend(title='Fruit color')
-
-errors = [3.063846976, 2, 3, 4, 13.76148297]
 
 plt.errorbar(xAxis, yAxis, yerr=errors, fmt="o", color="r")
 
